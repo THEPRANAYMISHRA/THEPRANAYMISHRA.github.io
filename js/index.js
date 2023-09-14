a0161d8ee96d7278f13bbbbef281a72b6c56e816
Pranay <pmcanvas4501@gmail.com>
2023-09-14 11:09:14 +0530

    chnages

--- js/index.js
+++ js/index.js
@@ -1,38 +1,35 @@
-let sections=document.querySelectorAll("section")
-let navlinks=document.querySelectorAll("header nav a")
+let sections = document.querySelectorAll("section")
+let navlinks = document.querySelectorAll("header nav a")
 
-window.onscroll=()=>{
-    sections.forEach(sec=>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop-150;
-        let height=sec.offsetHeight;
-        let id=sec.getAttribute('id');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset+height){
-            navlinks.forEach(links=>{
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
                 links.classList.remove('active')
-                document.querySelector('header nav a[href*=' +id+ ']').classList.add('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             })
         }
     })
 
-    let header=document.querySelector('header')
+    let header = document.querySelector('header')
 
-    header.classList.toggle('sticky',window.scrollY >100 )
+    header.classList.toggle('sticky', window.scrollY > 100)
 
     navbar.classList.remove('active')
 }
 
 const HamburgerMenuIcon = document.querySelector('#menu-icon');
-const navbar=document.querySelector(".navbar")
+const navbar = document.querySelector(".navbar")
 
-HamburgerMenuIcon.addEventListener('click', function() {
-  navbar.classList.toggle('active')
+HamburgerMenuIcon.addEventListener('click', function () {
+    navbar.classList.toggle('active')
 });
 
-// qcqwfjehczkdnfvo
-
 GitHubCalendar(".calendar", "THEPRANAYMISHRA");
-
 // or enable responsive functionality:
 GitHubCalendar(".calendar", "THEPRANAYMISHRA", { responsive: true });
